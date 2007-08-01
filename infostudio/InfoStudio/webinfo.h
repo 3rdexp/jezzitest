8b168d23a8cef51d54b022a92ee4421bb83e5c43
ken.shao <ken.shao@ba8f1dc9-3c1c-0410-9eed-0f8a660c14bd>
2007-08-01 14:58:34 +0000

    nothing changed

--- infostudio/InfoStudio/webinfo.h
+++ infostudio/InfoStudio/webinfo.h
@@ -2,6 +2,66 @@
 
 #include <map>
 #include <vector>
+#include <string>
+
+
+#if 0
+struct UserInfo
+{
+public:
+    typedef std::map<std::wstring, std::wstring> VarMap;    
+    VarMap vars;
+};
+
+
+enum SiteActionType
+{
+    SAT_REGISTER,   // 
+    SAT_LOGIN,
+    SAT_PUBLISH,
+    SAT_CHECK,
+};
+
+enum SiteCharset
+{
+    SC_ANSI,
+    SC_UTF8,
+};
+
+// for a=b&c={name}&e*= translate
+// usage:
+// VarTemplate vt(L"a=b&c={name}&e*=");
+// wstring s = vt.Apply(UserInfo.vars);
+// xxx.Post(s);
+
+class VarTemplate
+{
+public:
+    VarTemplate(const wstring & text);
+
+    typedef std::map<std::wstring, std::wstring> VarMap;
+
+    std::wstring Apply(const VarMap & vars);
+};
+
+// ???
+struct SiteAction
+{
+    SiteActionType type;
+    std::wstring url;
+    SiteCharset charset;
+    std::wstring referrer;
+};
+
+class SiteInfo
+{
+public:
+
+private:
+    vector<SiteAction> actions_; ???
+};
+
+#endif // #if 0
 
 // ? 行业需要模糊匹配，能在数据结构中直接表示否？
 // TODO:  std::vector< CategoryItem* > => std::vector<CategoryItem>
