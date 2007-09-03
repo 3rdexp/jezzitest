3d288fa72c3f7ff5ffbea6e7492a8a635bf966c6
ken.shao <ken.shao@ba8f1dc9-3c1c-0410-9eed-0f8a660c14bd>
2007-09-03 14:58:43 +0000

    one tree => left tree + right listview
    BaseData

--- branches/initialize/infostudio/studio/ypage.cpp
+++ branches/initialize/infostudio/studio/ypage.cpp
@@ -14,7 +14,8 @@ HTREEITEM RescurInsert(CTreeViewCtrl & tree, HTREEITEM parent, HTREEITEM after, 
     tvs.hInsertAfter = after;
     tvs.item.pszText = const_cast<wchar_t*>(ind.name.c_str());
     tvs.item.cchTextMax = ind.name.size();
-    tvs.item.mask = TVIF_TEXT;
+    tvs.item.mask = TVIF_TEXT | TVIF_PARAM;
+    tvs.item.lParam = ind.id;
 
     HTREEITEM ret = tree.InsertItem(&tvs);
     // ret = InsertItem(i->name.c_str(), parent, after);
@@ -26,14 +27,19 @@ HTREEITEM RescurInsert(CTreeViewCtrl & tree, HTREEITEM parent, HTREEITEM after, 
         inner_after = RescurInsert(tree, ret, inner_after, *i);
     }
 
+#ifdef ONE_TREE // test code
     if (ind.children.empty())
     {
         HTREEITEM hti = tree.InsertItem(L"site a", ret, inner_after);
         tree.SetItemData(hti, 1);
     }
+#endif
 
     return ret;
 }
+
+
+#ifdef ONE_TREE
 
 LRESULT SubYellowPage::OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/
                                 , BOOL& bHandled)
@@ -51,3 +57,53 @@ LRESULT SubYellowPage::OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lPara
     }
     return 0;
 }
+
+#else
+
+
+LRESULT SubYellowPage::OnCreate(UINT /*uMsg*/, WPARAM /*wParam*/, LPARAM /*lParam*/
+                                , BOOL& bHandled)
+{
+    // left tree
+    tv_.Create(m_hWnd, rcDefault, 0, WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE | WS_CHILD
+        | TVS_LINESATROOT | TVS_HASLINES | TVS_HASBUTTONS | TVS_DISABLEDRAGDROP 
+        | TVS_SHOWSELALWAYS);
+
+    // right list
+    lv_.Create(m_hWnd, rcDefault, 0, WS_CLIPCHILDREN | WS_CLIPSIBLINGS | WS_VISIBLE | WS_CHILD
+        | LVS_REPORT);
+
+    lv_.SetExtendedListViewStyle(LVS_EX_CHECKBOXES | LVS_EX_FULLROWSELECT);
+
+    m_cxyMin = 195;
+    SetSplitterPanes(tv_, lv_);
+
+    SetSplitterPos(195);
+
+    if (bd_) {
+        HTREEITEM parent = TVI_ROOT, after = TVI_FIRST;
+
+        Industry & ind = bd_->GetIndustry();
+        for (Industry::list_type::const_iterator i=ind.children.begin(); 
+            i != ind.children.end(); ++i)
+        {
+            after = RescurInsert(tv_, parent, after, *i);
+        }
+    }
+    return 0;
+}
+
+LRESULT SubYellowPage::OnTreeSelChanged(int /*idCtrl*/, LPNMHDR pnmh, BOOL& /*bHandled*/)
+{
+    NMTREEVIEW * ntv = (NMTREEVIEW *)pnmh;
+    int id = ntv->itemNew.lParam;
+    if (bd_)
+    {
+        // bd_->GetIndustry().find(id);
+    }
+    return 0;
+}
+
+#endif
+
+
