bc2d7641b99b7aba24a2c0dc8f336255d5997848
ken.shao <ken.shao@ba8f1dc9-3c1c-0410-9eed-0f8a660c14bd>
2010-09-14 14:44:42 +0000

    main.init warning fix

--- tags/square/main.py
+++ tags/square/main.py
@@ -66,6 +66,7 @@ class HomeHandler(base.BaseHandler):
     # self.set_header("Expires", "-1")
     self.render('home.html', feeds=feeds, user=user)
 
+
 class Square(tornado.web.Application):
   def __init__(self):
     handlers = [
@@ -114,19 +115,22 @@ class Square(tornado.web.Application):
       # self.db_ = pymongo.Connection('localhost', 27017).square
       self.db_ = pymongo.Connection(options.dbhost,  options.dbport).square
 
+
 tornado.options.define("port", default=1026, help="run on the given port", type=int)
 tornado.options.define("dbhost", default='localhost', help="mongodb host", type=str)
 tornado.options.define("dbport", default=27017, help="mongodb port", type=int)
+
 
 def init():
   db = pymongo.Connection(options.dbhost,  options.dbport).square
   # db.user.ensureIndex({c : '2d', n : 1}, {min:-180000, max:180000})
   
-  db.user.ensure_index('c', pymongo.GEO2D, unique=False, ttl=300,  min=-180000, max=180000)
+  db.user.ensure_index([('c', pymongo.GEO2D)], unique=False, ttl=300,  min=-180000, max=180000)
   db.user.ensure_index('n', unique=True)
   
+  db.feed.ensure_index([('where', pymongo.GEO2D)], unique=False, ttl=300,  min=-180000, max=180000)
   db.feed.ensure_index('time', unique=False)
-  db.feed.ensure_index('where', pymongo.GEO2D, unique=False, ttl=300,  min=-180000, max=180000)
+  
 
 def main():
   tornado.options.parse_command_line()
@@ -136,6 +140,7 @@ def main():
   ws = tornado.httpserver.HTTPServer(Square())
   ws.listen(options.port)
   tornado.ioloop.IOLoop.instance().start()
+
 
 if __name__ == "__main__":
   main()
